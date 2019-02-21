92a485af7074b1925a90c4afbbfc54e9a5453fdd
januwA <ajanuw1641845087@sina.com>
2019-02-21 17:24:20 +0800

    更新模板

--- db/scmb.js
+++ db/scmb.js
@@ -1,4 +1,4 @@
-module.exports = [{ // 订单支付成功通知
+const array = [{ // 订单支付成功通知, 预约成功
     store_tmpl_id: 'AT0009',
     keyid_num: '4',
     keyid1: '5',
@@ -12,64 +12,51 @@ module.exports = [{ // 订单支付成功通知
     keyid2: '5',
     keyid3: '4',
     keyid4: '17'
-  }
-
-  , { // 订单发货提醒
+  }, { // 订单发货提醒
     store_tmpl_id: 'AT0007',
-    keyid_num: '3',
-    keyid1: '5',
-    keyid2: '2',
-    keyid3: '23',
-  }
+    keyid_num: 4,
+    keyid1: 5,
+    keyid2: 2,
+    keyid3: 23,
+    keyid4: 106,
 
-  , { // 退款通知
+  }, { // 退款通知, 预约失败
     store_tmpl_id: 'AT0036',
-    keyid_num: '4',
-    keyid1: '3',
-    keyid2: '4',
-    keyid3: '30',
-    keyid4: '14',
-  }
-
-  , { // 拼团成功通知
+    keyid_num: 4,
+    keyid1: 33,
+    keyid2: 13,
+    keyid3: 3,
+    keyid4: 4,
+  }, { // 拼团成功通知
     store_tmpl_id: 'AT0051',
     keyid_num: '3',
     keyid1: '6',
     keyid2: '13',
     keyid3: '15',
-  }
-
-  , { // 拼团失败通知
+  },
+  { // 拼团失败通知
     store_tmpl_id: 'AT0310',
     keyid_num: '3',
     keyid1: '2',
     keyid2: '5',
     keyid3: '6',
-  }
-
-  , { // 提现到账通知
+  }, { // 提现到账通知
     store_tmpl_id: 'AT0830',
     keyid_num: '3',
     keyid1: '5',
     keyid2: '8',
     keyid3: '4',
-  }
-
-  , { // 提现失败通知
+  }, { // 提现失败通知
     store_tmpl_id: 'AT1242',
     keyid_num: '2',
     keyid1: '3',
     keyid2: '2',
-  }
-
-  , { // 审核状态通知
+  }, { // 审核状态通知
     store_tmpl_id: 'AT0674',
     keyid_num: '2',
     keyid1: '2',
     keyid2: '4',
-  }
-
-  , { // 账户变动提醒
+  }, { // 账户变动提醒
     store_tmpl_id: 'AT0677',
     keyid_num: '2',
     keyid1: '1',
@@ -85,5 +72,30 @@ module.exports = [{ // 订单支付成功通知
     keyid_num: 2,
     keyid1: 6,
     keyid2: 8,
+  },
+  {
+    // 报名成功通知
+    store_tmpl_id: 'AT0027',
+    keyid_num: 3,
+    keyid1: 6,
+    keyid2: 5,
+    keyid3: 18,
+  },
+  {
+    // 报名失败通知
+    store_tmpl_id: 'AT0028',
+    keyid_num: 3,
+    keyid1: 6,
+    keyid2: 1,
+    keyid3: 7,
+  },
+  {
+    // 中奖结果通知
+    store_tmpl_id: 'AT1186',
+    keyid_num: 2,
+    keyid1: 11,
+    keyid2: 6,
   }
 ]
+
+module.exports = array;
